d6fefe684e770f1396ffc5691adb7c49a705e207
scottkennethjackson <86068263+scottkennethjackson@users.noreply.github.com>
2024-05-05 17:58:45 +0100

    Build nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ["**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'playfair': ['Playfair Display', 'Georgia', 'serif'],
+        'source': ['Source Sans 3', 'Arial', 'sans-serif']
+      },
+
+      colors: {
+        transparent: 'transparent',
+        'balck': '#000000',
+        'white': '#ffffff',
+        'lightblue': '#b4ddd5',
+        'orange': '#f68330',
+        'yellow': '#f7c412',
+        'mediumblue': '#033756',
+        'darkblue': '#08143a',
+        'offwhite': '#eef0ee',
+        'activeyellow': '#deb110'
+      },
+
+      spacing: {
+        '50px': '50px'
+      },
+    },
   },
   plugins: [],
 }
