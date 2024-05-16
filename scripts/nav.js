f4482aa7d28bba8292ac4fa3e38d209aaa632adb
scottkennethjackson <86068263+scottkennethjackson@users.noreply.github.com>
2024-05-16 22:30:25 +0100

    Add nav dropdown

---
+++ scripts/nav.js
@@ -0,0 +1,49 @@
+const navbar = document.getElementById('navbar');
+const menuBtn = document.getElementById('menu-btn');
+const bar1 = document.getElementById('bar1');
+const bar2 = document.getElementById('bar2');
+const bar3 = document.getElementById('bar3');
+const dropdown = document.getElementById('dropdown');
+const sectionLink = document.querySelectorAll('.section-link');
+
+// When the user enters and leaves the menu-btn, add and remove the duration-700 class
+menuBtn.addEventListener('mouseenter', () => {
+    bar1.classList.add('duration-700');
+    bar2.classList.add('duration-700');
+    bar3.classList.add('duration-700');
+});
+
+menuBtn.addEventListener('mouseleave', () => {
+    bar1.classList.remove('duration-700');
+    bar2.classList.remove('duration-700');
+    bar3.classList.remove('duration-700');
+});
+
+// When the user clicks the menu-btn, translate bars and display the dropdown
+menuBtn.addEventListener('click', () => {
+    dropdown.classList.toggle('translate-y-full');
+    bar1.classList.toggle('rotate-45');
+    bar1.classList.toggle('translate-y-2');
+    bar2.classList.toggle('rotate-45');
+    bar3.classList.toggle('-rotate-45');
+    bar3.classList.toggle('-translate-y-2');
+});
+
+// When the user leaves the navbar or clicks a section-link, reset the menu-btn and dropdown
+navbar.addEventListener('mouseleave', () => {
+    dropdown.classList.remove('translate-y-full');
+    bar1.classList.remove('rotate-45');
+    bar1.classList.remove('translate-y-2');
+    bar2.classList.remove('rotate-45');
+    bar3.classList.remove('-rotate-45');
+    bar3.classList.remove('-translate-y-2');
+});
+
+sectionLink.forEach(link => link.addEventListener('click', () => {
+    dropdown.classList.remove('translate-y-full');
+    bar1.classList.remove('rotate-45');
+    bar1.classList.remove('translate-y-2');
+    bar2.classList.remove('rotate-45');
+    bar3.classList.remove('-rotate-45');
+    bar3.classList.remove('-translate-y-2');
+}));
