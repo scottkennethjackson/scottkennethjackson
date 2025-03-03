2c879c901d226ef57c9e7a17210811799697824a
scottkennethjackson <86068263+scottkennethjackson@users.noreply.github.com>
2025-03-03 14:44:03 +0000

    Add navbar functionality
    Wrote functions and added styling to make the navbar and dropdown more responsive

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,23 +1,18 @@
-const main = document.querySelector('main');
+const header = document.querySelector('header');
 const menuBtn = document.getElementById('menu-btn');
 const bar1 = document.getElementById('bar1');
 const bar2 = document.getElementById('bar2');
 const bar3 = document.getElementById('bar3');
 const dropdown = document.getElementById('dropdown');
+const navElement = document.querySelectorAll('.nav-element');
 const darkBtn = document.getElementById('dark-btn');
 const slider = document.getElementById('slider');
+const main = document.querySelector('main');
 
 menuBtn.addEventListener('mouseenter', () => {
     bar1.classList.add('duration-500');
     bar2.classList.add('duration-500');
     bar3.classList.add('duration-500');
-});
-
-// When the user leaves the menu-btn, remove the duration-700 class
-menuBtn.addEventListener('mouseleave', () => {
-    bar1.classList.remove('duration-500');
-    bar2.classList.remove('duration-500');
-    bar3.classList.remove('duration-500');
 });
 
 menuBtn.addEventListener('click', () => {
@@ -29,7 +24,52 @@ menuBtn.addEventListener('click', () => {
     dropdown.classList.toggle('translate-y-full');
 });
 
+header.addEventListener('mouseleave', () => {
+    bar1.classList.remove('rotate-45');
+    bar1.classList.remove('translate-y-2.5');
+    bar2.classList.remove('rotate-45');
+    bar3.classList.remove('-rotate-45');
+    bar3.classList.remove('-translate-y-2.5');
+    dropdown.classList.remove('translate-y-full');
+
+    setTimeout(() => {
+        bar1.classList.remove('duration-500');
+        bar2.classList.remove('duration-500');
+        bar3.classList.remove('duration-500');
+    }, 500);
+});
+
+navElement.forEach(element => element.addEventListener('click', () => {
+    bar1.classList.remove('rotate-45');
+    bar1.classList.remove('translate-y-2.5');
+    bar2.classList.remove('rotate-45');
+    bar3.classList.remove('-rotate-45');
+    bar3.classList.remove('-translate-y-2.5');
+    dropdown.classList.remove('translate-y-full');
+
+    setTimeout(() => {
+        bar1.classList.remove('duration-500');
+        bar2.classList.remove('duration-500');
+        bar3.classList.remove('duration-500');
+    }, 500);
+}));
+
 darkBtn.addEventListener('click', () => {
     main.classList.toggle('dark');
     slider.classList.toggle('-translate-x-9');
+
+    setTimeout(() => {
+        bar1.classList.remove('rotate-45');
+        bar1.classList.remove('translate-y-2.5');
+        bar2.classList.remove('rotate-45');
+        bar3.classList.remove('-rotate-45');
+        bar3.classList.remove('-translate-y-2.5');
+        dropdown.classList.remove('translate-y-full');
+
+        setTimeout(() => {
+            bar1.classList.remove('duration-500');
+            bar2.classList.remove('duration-500');
+            bar3.classList.remove('duration-500');
+        }, 500);
+    }, 100);
 });
